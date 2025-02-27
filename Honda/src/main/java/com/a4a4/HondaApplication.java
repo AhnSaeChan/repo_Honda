46faf05dded6121f3196e2d315c88f06f19cf1db
AhnSaeChan <sechan8896@gmail.com>
2025-02-27 15:42:34 +0900

    first

---
+++ Honda/src/main/java/com/a4a4/HondaApplication.java
@@ -0,0 +1,13 @@
+package com.a4a4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HondaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HondaApplication.class, args);
+	}
+
+}
---
+++ Honda/src/test/java/com/a4a4/HondaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a4a4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HondaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
